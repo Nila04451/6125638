1fb8ed9c862fda23fa4a63165903297189f4ab86
sennila <hellosmily22@gmail.com>
2025-07-09 11:59:44 +0530

    Delete TableCreation.sql

--- TableCreation.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE country (
-    code VARCHAR(2) PRIMARY KEY,
-    name VARCHAR(50)
-);
-
-INSERT INTO country VALUES ('IN', 'India');
-INSERT INTO country VALUES ('US', 'United States of America');
-INSERT INTO country VALUES ('FR', 'France');
-INSERT INTO country VALUES ('JP', 'Japan');
